47edb0d48cf27fda3f382f17e32accbae32c9fa4
root <root@DESKTOP-3O9A2NK.>
2025-01-09 17:17:11 -0300

    fix: correção de configuração

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
-const App = () => {
-  return <div>App React</div>;
-};
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
 
 export default App;
