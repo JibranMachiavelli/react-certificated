f863022e02bec047e435354929e1504a53fa181c
root <root@DESKTOP-3O9A2NK.>
2025-01-09 20:45:06 -0300

    feat: implementado hooks, create login and login

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Home from './Components/Home';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import { UserStorage } from './UserContext';
 import Login from './Components/Login/Login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -10,12 +11,14 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login/*" element={<Login />} />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -3,17 +3,24 @@ import LoginForm from './LoginForm';
 import LoginCreate from './LoginCreate';
 import LoginPasswordLost from './LoginPasswordLost';
 import LoginPasswordReset from './LoginPasswordReset';
-import { Routes, Route } from 'react-router';
+import { Routes, Route, Navigate } from 'react-router';
+import { UserContext } from '../../UserContext';
+import styles from './Login.module.css'
 
 const Login = () => {
+    const { login } = React.useContext(UserContext)
+
+    if (login === true) return <Navigate to="/conta" />
     return (
-        <div>
-            <Routes>
-                <Route path='/' element={<LoginForm />} />
-                <Route path='criar' element={<LoginCreate />} />
-                <Route path='perdeu' element={<LoginPasswordLost />} />
-                <Route path='/resetar' element={<LoginPasswordReset />} />
-            </Routes>
+        <div className={styles.login}>
+            <div className={styles.forms}>
+                <Routes>
+                    <Route path='/' element={<LoginForm />} />
+                    <Route path='criar' element={<LoginCreate />} />
+                    <Route path='perdeu' element={<LoginPasswordLost />} />
+                    <Route path='/resetar' element={<LoginPasswordReset />} />
+                </Routes>
+            </div>
         </div>
     );
 };
