b43658bca71a977e6fac61fd11dfc3139c02a408
root <root@DESKTOP-3O9A2NK.>
2025-01-09 18:51:11 -0300

    feat: implementado header footer components input e button e styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
 import './App.css';
+import React from 'react';
+import Home from './Components/Home';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import Login from './Components/Login/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Header />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login/*" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
+import Dogs from '../Assets/dogs.svg?react';
 
 const Header = () => {
   return (
-    <div className={styles.header}>
-      Header
-    </div>
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
+          <Dogs />
+        </Link>
+        <Link className={styles.login} to="/login">
+          Login / Criar
+        </Link>
+      </nav>
+    </header>
   );
 };
 
